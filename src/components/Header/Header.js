155f0b6fde71a13a162121b3ec9059c8d7c219a2
Allison <allisonjuliaking@gmail.com>
2024-06-18 12:06:06 -0400

    headers

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
-import MobileMenu from '../MobileMenu';
+import { COLORS, QUERIES, WEIGHTS } from "../../constants";
+import Logo from "../Logo";
+import SuperHeader from "../SuperHeader";
+import MobileMenu from "../MobileMenu";
+import Icon from "../Icon";
+import UnstyledButton from "../UnstyledButton";
+import VisuallyHidden from "../VisuallyHidden";
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -18,18 +21,32 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Side>
+        <LogoWrapper>
           <Logo />
-        </Side>
-        <Nav>
+        </LogoWrapper>
+        <DesktopNav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
           <NavLink href="/men">Men</NavLink>
           <NavLink href="/women">Women</NavLink>
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
+        </DesktopNav>
+        <MobileNav>
+          <UnstyledButton>
+            <Icon id="shopping-bag" />
+            <VisuallyHidden>Open cart</VisuallyHidden>
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon id="search" />
+            <VisuallyHidden>Search</VisuallyHidden>
+          </UnstyledButton>
+          <UnstyledButton onClick={() => setShowMobileMenu(true)}>
+            <Icon id="menu"></Icon>
+            <VisuallyHidden>Menu</VisuallyHidden>
+          </UnstyledButton>
+        </MobileNav>
+        <RightSide />
       </MainHeader>
 
       <MobileMenu
@@ -46,16 +63,53 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    align-items: center;
+  }
+
+  @media (${QUERIES.phoneAndSmaller}) {
+    padding-left: 16px;
+    padding-right: 16px;
+  }
 `;
 
-const Nav = styled.nav`
+const DesktopNav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
+  @media (${QUERIES.tabletAndSmaller}) {
+    display: none;
+  }
 `;
 
-const Side = styled.div`
+const MobileNav = styled.nav`
+  display: none;
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    display: flex;
+    align-items: center;
+    gap: 32px;
+  }
+  @media (${QUERIES.phoneAndSmaller}) {
+    gap: 16px;
+  }
+`;
+
+const LogoWrapper = styled.div`
   flex: 1;
+
+  @media (${QUERIES.tabletAndSmaller}) {
+    flex: revert;
+    margin-right: auto;
+  }
+`;
+
+const RightSide = styled.div`
+  flex: 1;
+  @media (${QUERIES.tabletAndSmaller}) {
+    display: none;
+  }
 `;
 
 const NavLink = styled.a`
